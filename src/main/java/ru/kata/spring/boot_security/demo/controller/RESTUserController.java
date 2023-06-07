3ec36f14470beea79401d625c95ab0456c5a3a7b
qmained <unconfigured@null.spigotmc.org>
2023-06-07 11:34:43 +0300

    initial commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RESTUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RESTUserController.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
